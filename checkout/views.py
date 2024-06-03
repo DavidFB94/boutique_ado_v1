141637b54d104b134459a20faf23d9d5428dea99
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-03 09:28:17 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PNWQYP4pHPk2HEdCMB7qspSGtdAuseuIlenEv1M7OXVaGUsnV59Hl52cU0OauZNVf5oHHy3oTVxfloxiJJx6CXZ00tKuU4jdC',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
